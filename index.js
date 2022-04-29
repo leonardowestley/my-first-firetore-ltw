0c23509d60a8ef10d1ab433039c3472f057f37d5
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-04-29 11:34:40 -0400

    adding data two ways

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-
 const restaurant = {
     name: 'Mister 01',
     address: '555 N Federal Hwy Suite',
@@ -16,6 +15,24 @@ const restaurant = {
     rating: 4.9,
     phone: '(786) 6787-2903',
 }
-db.collection('restaurants').add(restaurant)
-.then(doc => console.log('Created restaurant', doc.id))
-.catch(err => console.error(err))
+
+// db.collection('restaurants').add(restaurant)
+// .then(doc => console.log('Created restaurant', doc.id))
+// .catch(err => console.error(err))
+
+
+const restaurant2 = {
+    name: 'Bolay',
+    cuisine: 'American',
+    rating: 4.6,
+}
+
+async function addRestaurant() {
+   try {
+    const doc = await db.collection('restaurants').add(restaurant2)
+    console.log('Created restaurant', doc.id)
+ } catch(err) {
+
+ }
+}
+addRestaurant(restaurant2)
