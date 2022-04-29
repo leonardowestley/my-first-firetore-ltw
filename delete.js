0656319da0c80e0647635e581e7136275908df2e
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-04-29 13:57:00 -0400

    changed credentials

---
+++ delete.js
@@ -0,0 +1,19 @@
+// Created restaurant 
+// Created restaurant
+
+const admin = require("firebase-admin"); //imports firebase library of tools
+
+const serviceAccount = require('./credentials.json');//import our creds 2 connect 2 firebase
+
+admin.initializeApp({ //connects to our firebase project
+  credential: admin.credential.cert(serviceAccount)// creating a certificate from our creds
+});
+
+// Now we are connected to OUR Firebase project & related services
+
+const db = admin.firestore(); // creates shortcut to access the Firestore database
+
+// delete 
+restaurantsCol.doc('id').delete()
+.then(res => console.log('success deleting id'))
+.catch(err => console.error('error deleting id', err))
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const admin = require("firebase-admin");
+const admin = require("firebase-admin"); //imports firebase library of tools
 
-const serviceAccount = require('./credentials.json');
+const serviceAccount = require('./credentials.json');//import our creds 2 connect 2 firebase
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+admin.initializeApp({ //connects to our firebase project
+  credential: admin.credential.cert(serviceAccount)// creating a certificate from our creds
 });
 
-const db = admin.firestore();
+// Now we are connected to OUR Firebase project & related services
+
+const db = admin.firestore(); // creates shortcut to access the Firestore database
 
 const restaurant = {
     name: 'Mister 01',
     address: '555 N Federal Hwy Suite',
-    cusisine: 'Pizza',
+    cuisine: 'Pizza',
     rating: 4.9,
     phone: '(786) 6787-2903',
-}
+} // creating an object -- in this case a specific restaurant we want to add to our
 
-// db.collection('restaurants').add(restaurant)
-// .then(doc => console.log('Created restaurant', doc.id))
-// .catch(err => console.error(err))
+// db.collection('restaurants').add(restaurant) // adding the restaurant to our restaurants collection
+// .then(doc => console.log('Created restaurant', doc.id))// handling resolve
+// .catch(err => console.error(err)) // handling reject
 
 
 const restaurant2 = {
@@ -30,9 +32,9 @@ const restaurant2 = {
 
 async function addRestaurant() {
    try {
-    const doc = await db.collection('restaurants').add(restaurant2)
-    console.log('Created restaurant', doc.id)
- } catch(err) {
+    const doc = await db.collection('restaurants').add(restaurant2) // add restaurant to collection
+    console.log('Created restaurant', doc.id)// handle resolve
+ } catch(err) { // handle reject
 
  }
 }
